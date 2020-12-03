6ab892487e17f128d1a777820474287c2465e2b8
Oskar Andersson <58656584+oskand01@users.noreply.github.com>
2020-12-03 20:15:50 +0100

    Update oskar_andersson.js

--- Inlämningsuppgift 2/oskar_andersson.js
+++ Inlämningsuppgift 2/oskar_andersson.js
@@ -1,1 +1,58 @@
 "use strict";
+
+//user can only login with these names.
+let usernames = ["yazeen", "ec", "softhouse"];
+
+let form = document.createElement("form");
+let input = document.createElement("input");
+
+//Function that calls all other functions in order
+//--Don't modify--
+function init() {
+  initLoginForm();
+  initLabel();
+  initInput();
+  initButton();
+}
+
+function initLoginForm() {
+  document.body.appendChild(form);
+}
+
+function initLabel() {
+  let label = document.createElement("label");
+  label.textContent = "username";
+  form.appendChild(label);
+}
+
+function initInput() {
+  input.required = true;
+  form.appendChild(input);
+}
+
+function initButton() {
+  let button = document.createElement("button");
+  button.textContent = "login";
+  form.appendChild(button);
+
+  button.addEventListener("click", () => {
+    if (validateLoginInfo() === true) {
+      alert("Welcome");
+    } else {
+      input.style.border = "3px solid red";
+    }
+  });
+}
+
+function validateLoginInfo() {
+  for (let i = 0; i < usernames.length; i++) {
+    if (usernames[i] === input.value) {
+      return true;
+    }
+  }
+  return false;
+}
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  init();
+});
